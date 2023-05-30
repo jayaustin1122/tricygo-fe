ab06dd002993631104c7080b74f80375e459d794
Lander Rieza Villagracia <villagracialander@gmail.com>
2023-05-30 20:20:19 +0800

    feat: update admin page products

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,156 +1,191 @@
 <template>
   <div>
-    <h1>ORDERS HISTORY</h1>
+    <h1>COSTUMER ORDERS</h1>
     <div class="d-flex mt-3">
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined  >
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
-                </v-list-item>
-              <v-card-actions>
-          </v-card-actions>
-      </v-card>
-    </div>
-    <div class="d-flex">
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
-        <v-list-item three-line> 
-              <v-list-item-content>
-                <div >
-                  <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
                 </div>
-                  <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
-                  <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
-                </div>
-                  </v-list-item-content>
-                </v-list-item>
-              <v-card-actions>
-          </v-card-actions>
-      </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
-        <v-list-item three-line> 
-              <v-list-item-content>
-                <div >
-                  <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
-                </div>
-                  <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
-                  <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
-                </div>
-                  </v-list-item-content>
-                </v-list-item>
-              <v-card-actions>
-          </v-card-actions>
-      </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
-        <v-list-item three-line> 
-              <v-list-item-content>
-                <div >
-                  <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
-                </div>
-                  <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
-                  <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
-                </div>
-                  </v-list-item-content>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
     </div>
-    <div class="d-flex">
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
+    <div class="d-flex mt-3">
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: green;">Pending</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: blue;">Delivered</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
-      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="344" outlined >
+      <v-card class="mx-5 boxbg   rounded-card mt-3" max-width="400" outlined  >
         <v-list-item three-line> 
               <v-list-item-content>
                 <div >
+                  <v-list-item-title class="text-h5" style="color: black;">Sample Name</v-list-item-title>
                   <v-list-item-title class="text-h5" style="color: black;">Ordinary</v-list-item-title>
                 </div>
                   <v-list-item-subtitle><h4 style="color: black;">₱40,000</h4></v-list-item-subtitle>
-                  <div class="d-flex">
+                  <div>
                   <v-list-item-title style="color: black;">Status:</v-list-item-title>
-                  <v-list-item-title style="color: red;">Cancelled</v-list-item-title>
+                  <v-list-item-title style="color: green;">Delivered</v-list-item-title>
                 </div>
-                  </v-list-item-content>
+                <div class="d-flex align-items-center">
+                  <div class="text-center">
+                    <v-btn
+                      class="mx-2" fab dark small color=blue><v-icon dark>mdi-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=green><v-icon dark>mdi-package-variant-closed-check</v-icon>
+                    </v-btn>
+                    <v-btn
+                      class="mx-2" fab dark small color=red><v-icon dark>mdi-cancel</v-icon>
+                    </v-btn>
+                  </div>
+                </div>
+              </v-list-item-content>
                 </v-list-item>
               <v-card-actions>
           </v-card-actions>
       </v-card>
     </div>
+    
   </div>
 </template>
 
@@ -162,6 +197,7 @@ export default {
 <style scoped>
 h1{
   color: #50C878;
+  
 }
 .gg{
   align-items: center;
